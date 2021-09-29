2798baf689ebe91da365735b04f1fbd023f47752
Nicolas Zucconi <nicolaszucconi@gmail.com>
2021-09-29 20:38:57 -0300

    Changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // All Products
 class Product {
-        constructor(name, size, price, stock) {
-            this.name = name;
-            this.size  = size;
-            this.price  = parseFloat(price);
-            this.stock  = parseInt(stock);
-        }
+    constructor(name, size, price, stock) {
+        this.name = name;
+        this.size  = size;
+        this.price  = parseFloat(price);
+        this.stock  = parseInt(stock);
+    }
 }
 
 // All Transactions
@@ -24,6 +24,18 @@ class Cashier {
         return this.totalPurchase * this.cardDiscount;
     }
 
+    getProduct() {
+
+        $.ajax({
+           method: "GET",
+           url: "test.json"
+        })
+    
+        .done(function(response) {
+            console.log(response);
+        });
+    }
+
     calculateChange(payment, subtotal) {
         return payment - this.totalPurchase;
     }
@@ -39,38 +51,36 @@ const productShirts = [
 const productHats = [
         hat1 = new Product("Join", "Only Size", 85, 4),
         hat2 = new Product("Just Us", "Only Size", 150, 4),
- ]
+    ]
 
 const cashier = new Cashier();
 
 let insertShirts = $(() => {
     $("#shirts").on("click", () => {
-    cashier.scanProduct(shirt1)
-    console.log("Product added to the cart")
-    localStorage.setItem('Product', JSON.stringify(productShirts))
+        cashier.scanProduct(shirt1)
+        console.log("Product added to the cart")
+        localStorage.setItem('Product', JSON.stringify(productShirts))
     })
 });
 
 let insertHats = $(() => {
     $("#hats").on("click", () => {
-    cashier.scanProduct(hat2);
-    console.log("Product added to the cart")
+        cashier.scanProduct(hat2);
+        console.log("Product added to the cart")
     })
 })
 
 // Payment Methods
 
-$("goToCart").on("click", (e) => {
-    e.preventDefault();
-    console.log("The total of your purchase is " + cashier.totalPurchase);
+$("#goToCart").on("click", () => {
     let cardPayment = confirm("Do you wish to pay with a credit card and get 20% discount?");
+    console.log("The total of your purchase is " + cashier.totalPurchase);
     
     if (cardPayment) { 
     // Card Method
-    cashier.totalPurchase = cashier.getTotalWithDiscount();
-    console.log("Su total es de " + cashier.totalPurchase);
-    } 
-        else {
+        cashier.totalPurchase = cashier.getTotalWithDiscount();
+        console.log("Su total es de " + cashier.totalPurchase);
+    } else {
         // Cash Method
         console.log("The total of your purchase is " + cashier.totalPurchase);
         let newPayment = Number (prompt("Ingrese Pago"));
@@ -81,7 +91,8 @@ $("goToCart").on("click", (e) => {
 
 let priceList = $(() => {
     $("#priceList").on("click", () => {
-    const productList = productShirts.concat(productHats)
+    // const productList = productShirts.concat(productHats)
+    const productList = cashier.getProduct();
     console.log(productList);
     })
 })
